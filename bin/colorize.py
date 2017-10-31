495a25efc61a142e895013dd6f335ecc69981501
Fritz Reese <Reese-Fritz@zai.com>
2017-10-30 20:58:45 -0400

    Add -i for interactive mode with colorize, allowing for unbuffered input reads

--- bin/colorize.py
+++ bin/colorize.py
@@ -6,6 +6,12 @@
 # Simple sed-like script to filter terminal color escapes into any text stream.
 #
 
+def tdbg(msg):
+    import threading
+    me = threading.current_thread().name
+    sys.stdout.write('[%s] %s\n' % (me, msg))
+
+import select
 import signal
 import sys
 import re
@@ -95,7 +101,7 @@ color. If the regex has a match group (parenthesis), only the [first] group
 will be colorized.
 
 {options}:
-    none yet
+    -i	interactive mode: do not line-buffer the input
 
 {colorspecs}:
     -r	{r}		-R	{br}		-k	{k}
@@ -145,8 +151,111 @@ def sigpipe(*args):
     sys.stdout.close ()
     sys.exit (0)
 
+_libc = None
+_sched_yield = None
+def sched_yield(timeout=0.010):
+    """Reliable wrapper around sched_yield() from libc which falls-back to
+    time.sleep(timeout) if sched_yield cannot be found from libc.so.6.
+    Note the timeout argument is only relevant the first time this wrapper
+    is called, as it binds the timeout to the function on load for speed."""
+    global _sched_yield
+    global _libc
+    if _sched_yield is None:
+        try:
+            import ctypes
+            if _libc is None:
+                _libc = ctypes.CDLL('libc.so.6')
+            _sched_yield = _libc.sched_yield
+        except OSError:
+            import time
+            _sched_yield = lambda : time.sleep(timeout)
+    return _sched_yield()
+
+class unbuffered_stream(object):
+    def __init__(self, istream):
+        self.infd = istream.fileno()
+        self.mask = select.POLLIN|select.POLLHUP|select.POLLNVAL|select.POLLERR
+        self.poller = select.poll()
+        self.poller.register(self.infd, self.mask)
+        self.istream = istream
+        self.closed = False
+
+    def _get_chars(self):
+        """Return all characters available on the input stream at this moment.
+        Basically polls the input repeatedly, reading one character at a time
+        until the input runs out of characters.
+        If no characters are available right now, return the empty string.
+        If the stream is closed or encountered an error, return None."""
+        if self.closed:
+            return None
+
+        # When we _first_ check for characters, it is okay to wait for some to
+        # appear. It is only once a set of characters appear on the input that
+        # we want to eat them all with no timeout and return them.
+        line = StringIO()
+        check_list = self.poller.poll(1)
+        while check_list and not self.closed:
+            # The poller should have at most one element for istream.
+            fd, event = check_list[0]
+            # assert(fd == self.infd)
+
+            # Collect characters into the line one-by-one.
+            # See if we have any more characters for next time.
+            if event & select.POLLIN != 0:
+                line.write(self.istream.read(1))
+                check_list = self.poller.poll(0)
+
+            # For any other event, the stream must be dead.
+            elif event != 0:
+                self.closed = True
+
+        # Return any characters we have found so far.
+        # Even if we are done, we should return any characters we read
+        # this time, then return None next time (remembered by self.closed).
+        if line:
+            ret = line.getvalue()
+            line.close()
+            return ret
+
+        # Once we are done, if we have no input, return None.
+        if self.closed:
+            return None
+
+        # If we got no input but still aren't done, just return empty string.
+        return ''
+
+    def __iter__(self):
+        """Return an iterator yielding a sequence of unbuffered input
+        characters roughly as they become available on the input stream.  The
+        stream must support the fileno() operation and polling, and the output
+        order is subject to CPU scheduling. The colorizing might be off if a
+        pattern would require matching characters which end up split between
+        two items. This is mostly for small, simple patterns in an interactive
+        program (such as highlighting the prompt in an interactive Python
+        session).
+        """
+        # Get available input characters in chunks.  Whence line is None, the
+        # stream has been shutdown or experienced an error
+        line = self._get_chars()
+        while line is not None:
+            # Yield non-empty sequences.
+            # Empty sequences may appear when the stream has nothing for us yet
+            # but we greedily checked anyway.
+            if line:
+                yield line
+            # Don't eat the CPU spinning on empty input.
+            sched_yield()
+            line = self._get_chars()
+
+def select_lines(istream):
+    """Return an iterator yielding a sequence of [line-buffered] input lines.
+    Note if the input never ends with a new-line, it will never be yielded."""
+    line = istream.readline()
+    if line:
+        yield line
+
 # each color gets an optional regex argument
-_sopts = 'h'+'::'.join(_kcolors)+'::'
+_sopts = 'hi'+'::'.join(_kcolors)+'::'
 
 def parse_args(args=None):
     """Parse the arguments according to our usage and return (opts, args)
@@ -164,7 +273,7 @@ def colorize(opts, istream=None, ostream=None):
 
     # list of ColorMatch objects
     results = list()
-
+    line_iter = select_lines
     for opt, optarg in opts:
         if opt[1] in _kcolors and opt[1].isalpha():
             rx = re.compile (optarg)
@@ -174,13 +283,12 @@ def colorize(opts, istream=None, ostream=None):
                 usage("bad regex: '%s'" % (optarg,))
         elif opt[1] == 'h':
             usage()
+        # Interactive mode - use unbuffered (non-blocking) reads
+        elif opt[1] == 'i':
+            line_iter = unbuffered_stream
 
     # copy input to output, filtering colors along the way
-    while True:
-        line = istream.readline ()
-        if not line:
-            break
-
+    for line in line_iter(istream):
         for cmatch in results:
             line = cmatch.filter(line)
 
